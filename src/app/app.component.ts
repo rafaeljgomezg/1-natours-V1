24afd3116120c1531a82faffe98350b2dcb86ebd
rafaeljgomezg <rafaeljgomezg@gmail.com>
2024-08-10 18:58:35 -0500

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { AboutComponent } from "./components/about/about.component";
+import { FeatureComponent } from "./components/feature/feature.component";
+import { ToursComponent } from "./components/tours/tours.component";
+import { StoriesComponent } from "./components/stories/stories.component";
+import { BookingComponent } from "./components/booking/booking.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { PopupComponent } from "./components/popup/popup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, FeatureComponent, ToursComponent, StoriesComponent, BookingComponent, FooterComponent, NavigationComponent, PopupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
